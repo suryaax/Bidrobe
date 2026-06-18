3babe787614e6dc37604c90343a051ab79c45866
suryaax <fadilsuryaibrani@gmail.com>
2026-06-18 19:56:29 +0700

    fix translated alert language

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -93,7 +93,7 @@ router.post("/login", async (req, res) => {
 
     if (!isMatch) {
       return res.status(400).json({
-        message: "Username/Kata sandi salah",
+        message: "Email/Kata sandi salah",
       });
     }
 
